b410a0ce1040cae301e637448e6bff43f50efff8
tcvduc <caovanducs@gmail.com>
2023-06-28 08:34:04 +0700

    update github access token

--- forge.config.js
+++ forge.config.js
@@ -30,4 +30,17 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "github-user-name",
+          name: "github-repo-name",
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
 };
